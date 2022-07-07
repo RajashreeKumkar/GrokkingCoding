e6b87c500df36856f295b1ea8f43df51f6b861ea
Rajashree Kumkar <rajashree.kumkar@ihsmarkit.com>
2022-07-07 13:51:14 -0500

    add

--- Patterns/1.SlidingWindow/1.MaximumSumSubarrayofSizeK.py
+++ Patterns/1.SlidingWindow/1.MaximumSumSubarrayofSizeK.py
@@ -12,14 +12,20 @@ Explanation: Subarray with maximum sum is [5, 1, 3].
 
 
 def maximumSum(arr,k):
-
-
-
-
+    res=0
+    start=0
+    window_sum=0
+    for end in range(len(arr)):
+        window_sum += arr[end]
+        if end>k-1:
+            window_sum-=arr[start]
+            start+=1
+        res=max(res, window_sum)
+    return res;
 
 
 if __name__ =='__main__':
     #arr=[2,1,5,2,3,2]
     #arr = [2, 1, 5, 2, 8]
-    arr = [3,4,1,1,6]
-    print(maximumSum(arr, 2))
+    arr = [2, 1, 5, 1, 3, 2]
+    print(maximumSum(arr, 3))
